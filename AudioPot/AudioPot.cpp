86afa3ac7553053de5feb704dc760aaf123add7a
Valentin-Gabriel Radu <valentin.radu@valinet.ro>
2020-08-26 03:04:22 +0300

    Initial commit

---
+++ AudioPot/AudioPot.cpp
@@ -0,0 +1,385 @@
+#define _WIN32_DCOM
+#include <Windows.h>
+#include <iostream>
+#include <mmdeviceapi.h>
+#include <endpointvolume.h>
+#include <conio.h>
+#include <Dbt.h>
+
+#define BUFFER_SIZE 6
+
+#define APPLICATION_NAME TEXT("AudioPot")
+
+//BOOL filterFirstMute = TRUE;
+
+// https://stackoverflow.com/questions/50722026/how-to-get-and-set-system-volume-in-windows
+float GetSystemVolume() {
+    HRESULT hr;
+
+    // -------------------------
+    CoInitialize(NULL);
+    IMMDeviceEnumerator* deviceEnumerator = NULL;
+    hr = CoCreateInstance(
+        __uuidof(MMDeviceEnumerator), 
+        NULL, 
+        CLSCTX_INPROC_SERVER, 
+        __uuidof(IMMDeviceEnumerator), 
+        (LPVOID*)&deviceEnumerator
+    );
+    IMMDevice* defaultDevice = NULL;
+
+    hr = deviceEnumerator->GetDefaultAudioEndpoint(
+        eRender, 
+        eConsole, 
+        &defaultDevice
+    );
+    deviceEnumerator->Release();
+    deviceEnumerator = NULL;
+
+    IAudioEndpointVolume* endpointVolume = NULL;
+    hr = defaultDevice->Activate(
+        __uuidof(IAudioEndpointVolume), 
+        CLSCTX_INPROC_SERVER, 
+        NULL, 
+        (LPVOID*)&endpointVolume
+    );
+    defaultDevice->Release();
+    defaultDevice = NULL;
+
+    float currentVolume = 0;
+    //Current volume as a scalar
+    hr = endpointVolume->GetMasterVolumeLevelScalar(&currentVolume);
+    endpointVolume->Release();
+    CoUninitialize();
+
+    return currentVolume;
+}
+
+void process(char* szBuffer)
+{
+    //printf("%s\n", szBuffer);
+    // I have to use ProgMan so that the changes work when the user is
+    // switched to another desktop as well - keybd_event does not work
+    // when the keyboard is active on another desktop, for example
+    LPARAM lParam;
+    WPARAM wParam;
+    if (szBuffer[0] != 0 && szBuffer[0] != 'm')
+    {
+        DWORD dwVal = 100 * atoi(szBuffer) / 1023;
+        float dwVol = GetSystemVolume() * 100;
+        int dwCnt = (dwVal - dwVol) / 2;
+        if (dwCnt < 0)
+        {
+            lParam = APPCOMMAND_VOLUME_DOWN;
+            dwCnt = 0 - dwCnt;
+        }
+        else
+        {
+            lParam = APPCOMMAND_VOLUME_UP;
+        }
+        wParam = (WPARAM)FindWindow(L"ProgMan", NULL);
+        for (DWORD i = 0; i < dwCnt; ++i)
+        {
+            SendMessage(
+                (HWND)wParam,
+                WM_APPCOMMAND,
+                wParam,
+                lParam * 65536
+            );
+        }
+    }
+    else if (szBuffer[0] != 0)
+    {
+        //if (!filterFirstMute)
+        //{
+            wParam = (WPARAM)FindWindow(L"ProgMan", NULL);
+            SendMessage(
+                (HWND)wParam,
+                WM_APPCOMMAND,
+                wParam,
+                APPCOMMAND_MEDIA_PLAY_PAUSE * 65536
+            );
+        //}
+        //filterFirstMute = FALSE;
+    }
+}
+
+DWORD WINAPI ServiceWorkerThread
+(
+    LPVOID lpParam
+)
+{
+    HANDLE hComm;
+    hComm = CreateFile(
+        L"\\\\.\\COM240",
+        GENERIC_READ,
+        0,
+        0,
+        OPEN_EXISTING,
+        FILE_FLAG_OVERLAPPED,
+        0
+    );
+    if (hComm == INVALID_HANDLE_VALUE)
+    {
+        printf("CreateFile: %d\n", GetLastError());
+        return NULL;
+    }
+    //printf("opening serial port successful.\n");
+
+    // Retrieve and configure configuration parameters
+    DCB dcbSerialParams = { 0 };
+    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+    if (!GetCommState(hComm, &dcbSerialParams))
+    {
+        printf("GetCommState: %d\n", GetLastError());
+        CloseHandle(hComm);
+        return NULL;
+    }
+    dcbSerialParams.BaudRate = CBR_9600;
+    dcbSerialParams.ByteSize = 8;
+    dcbSerialParams.StopBits = ONESTOPBIT;
+    dcbSerialParams.Parity = NOPARITY;
+    if (!SetCommState(hComm, &dcbSerialParams))
+    {
+        printf("SetCommState: %d\n", GetLastError());
+        CloseHandle(hComm);
+        return NULL;
+    }
+
+    COMMTIMEOUTS comTimeOut = { 0 };
+    comTimeOut.ReadIntervalTimeout = 0;
+    comTimeOut.ReadTotalTimeoutMultiplier = 0;
+    comTimeOut.ReadTotalTimeoutConstant = 0;
+    if (!SetCommTimeouts(hComm, &comTimeOut))
+    {
+        printf("SetCommTimeouts: %d\n", GetLastError());
+        CloseHandle(hComm);
+        return NULL;
+    }
+
+    OVERLAPPED overlapped = { 0 };
+    overlapped.hEvent = CreateEvent(
+        NULL,
+        TRUE,
+        FALSE,
+        NULL
+    );
+    if (!overlapped.hEvent)
+    {
+        printf("CreateEvent: %d\n", GetLastError());
+        CloseHandle(hComm);
+        return NULL;
+    }
+
+    char szBuffer[BUFFER_SIZE];
+    ZeroMemory(szBuffer, BUFFER_SIZE);
+    DWORD dwPos = 0;
+    while (TRUE)
+    {
+        // https://stackoverflow.com/questions/15136645/serialports-and-waitformultipleobjects
+        // http://www.egmont.com.pl/addi-data/instrukcje/standard_driver.pdf
+
+        BOOL bRet;
+        DWORD dwBytesRead, dwRet;
+        char chRd;
+
+        bRet = ReadFile(
+            hComm,
+            &chRd,
+            1,
+            NULL,
+            &overlapped);
+        DWORD err = GetLastError();
+        if (!bRet && err != ERROR_IO_PENDING)
+        {
+            printf("ReadFile: %d\n", err);
+            break;
+        }
+        dwRet = WaitForSingleObject(
+            overlapped.hEvent,
+            INFINITE
+        );
+        if (dwRet == WAIT_OBJECT_0 + 0)
+        {
+            if (!GetOverlappedResult(
+                hComm,
+                &overlapped,
+                &dwBytesRead,
+                FALSE
+            ))
+            {
+                printf("GetOverlappedResult: %d\n", GetLastError());
+                break;
+            }
+            if (chRd == '\r')
+            {
+                process(szBuffer);
+                ZeroMemory(szBuffer, BUFFER_SIZE);
+                dwPos = 0;
+            }
+            else if (chRd != '\n')
+            {
+                szBuffer[dwPos] = chRd;
+                if (dwPos < BUFFER_SIZE - 1) dwPos++;
+            }
+        }
+        else
+        {
+            printf("WaitForSingleObject: %d\n", GetLastError());
+            break;
+        }
+    }
+
+    CloseHandle(overlapped.hEvent);
+    CloseHandle(hComm);
+    return NULL;
+}
+
+LRESULT CALLBACK WindowProc(
+    _In_ HWND   hwnd,
+    _In_ UINT   uMsg,
+    _In_ WPARAM wParam,
+    _In_ LPARAM lParam
+)
+{
+    switch (uMsg)
+    {
+    case WM_DEVICECHANGE:
+    {
+        switch (wParam)
+        {
+        case DBT_DEVICEARRIVAL:
+        {
+            DEV_BROADCAST_HDR* hdr = (DEV_BROADCAST_HDR*)lParam;
+            if (hdr->dbch_devicetype == DBT_DEVTYP_PORT)
+            {
+                DEV_BROADCAST_PORT* port = (DEV_BROADCAST_PORT*)lParam;
+                if (!wcscmp(port->dbcp_name, TEXT("COM240")))
+                {
+                    wprintf(TEXT("%s\n"), port->dbcp_name);
+                    PostQuitMessage(0);
+                }
+            }
+            break;
+        }
+        }
+        break;
+    }
+    }
+    return DefWindowProc(
+        hwnd,
+        uMsg,
+        wParam,
+        lParam
+    );
+}
+
+int WINAPI wWinMain(
+    HINSTANCE hInstance, 
+    HINSTANCE hPrevInstance, 
+    PWSTR pCmdLine, 
+    int nCmdShow
+)
+{
+#ifdef _DEBUG
+    FILE* conout;
+    AllocConsole();
+    freopen_s(
+        &conout,
+        "CONOUT$",
+        "w",
+        stdout
+    );
+#else
+    if (AttachConsole(ATTACH_PARENT_PROCESS)) {
+        FILE* f, *g;
+        freopen_s(
+            &f, 
+            "CONOUT$", 
+            "w", 
+            stdout
+        );
+        freopen_s(
+            &g, 
+            "CONOUT$", 
+            "w", 
+            stderr
+        );
+    }
+#endif
+
+    WNDCLASS wndClass = { 0 };
+    wndClass.lpfnWndProc = WindowProc;
+    wndClass.hInstance = hInstance;
+    wndClass.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_BACKGROUND);
+    wndClass.lpszClassName = APPLICATION_NAME;
+
+    HWND hWnd = CreateWindowEx(
+        NULL,
+        reinterpret_cast<LPCWSTR>(
+            MAKEINTATOM(
+                RegisterClass(&wndClass)
+            )
+            ),
+        APPLICATION_NAME,
+        NULL,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    MSG msg;
+    BOOL bRet;
+    while (TRUE)
+    {
+        //filterFirstMute = TRUE;
+        // Try to communicate with audio pot
+        HANDLE handle = CreateThread(
+            NULL,
+            0,
+            ServiceWorkerThread,
+            NULL,
+            0,
+            NULL
+        );
+        if (!handle)
+        {
+            break;
+        }
+        DWORD dwResult = WaitForSingleObject(
+            handle,
+            INFINITE
+        );
+        // Communication ended, check if error
+        if (dwResult == WAIT_FAILED)
+        {
+            break;
+        }
+        // If not, wait until the device is plugged into the system
+        while ((bRet = GetMessage(
+            &msg, 
+            NULL, 
+            0,         // should work with WM_DEVICECHANGE here
+            0)) != 0)  // and here, but it does not...
+        {
+            // An error occured
+            if (bRet == -1)
+            {
+                break;
+            }
+            // Device connected, will send WM_QUIT to retry communication
+            else
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+    }
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # AudioPot
-Provides Arduino code for reporting potentiometer values and a Windows service for interpreting and acting on the received data
+
+AudioPot is the software implementation for my audio volume knob project. Basically, I connected a potentiometer to an Arduino, and I output the values I read off it with the ADC to the serial port. On the client (works on Windows), I set the system volume proportional to the received value. Also, the potentiometer can be pressed down, triggering the reset button on the Arduino, which plays/pauses current media on the computer
+
+#### Concepts shown in this example:
+
+* Basic Arduino example of analogRead
+* Receiving device tree change notifications via the window message queue in Windows (I tried to use WMI, succeeded, but some sort of bug made the CPU usage of WMI Provider Host rise to 100%; also, why bother with all that overhead when doing it via the message queue is so elegant)
+* Reading data form the serial port asynchronously (using overlapped I/O) - not really necessary for the current state of the project, but I was planning something bigger
+* Persisting the daemon by implementing a Windows service for it (the service spawns a second process on the *WinSta0* station so that the application is able to change volume)
+* Changing volume in Windows -  sounds trivial, but you'd never guess...
+
+## Structure
+
+The project is divided in:
+
+* AudioPotArduino - Arduino code of the project, I tested it on an Arudino Nano
+* AudioPotService - a service that starts up the daemon as soon as possible and restarts it in case of failure
+* AudioPot - main app, communicates with Arduino and changes volume accordingly, if needed
